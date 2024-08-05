eed63bcfcbb4a06c623ad4cf9ae31fa22c860c5b
Marek Zelinka <mzelinka17@gmail.com>
2024-08-05 18:01:20 +0200

    setup redux

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { legacy_createStore as createStore } from 'redux'
 import App from './App.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const noteReducer = (state = [], action) => {
+  switch (action.type) {
+    case 'NEW_NOTE': {
+      state.push(action.payload)
+      return state
+    }
+    default: {
+      return state
+    }
+  }
+}
+
+const store = createStore(noteReducer)
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+const render = () =>
+  root.render(
+    <React.StrictMode>
+      <App store={store} />
+    </React.StrictMode>,
+  )
+
+render()
+store.subscribe(render)
