033fc73b4474c36ce90a26a11eb38ed021dd1628
Marek Zelinka <mzelinka17@gmail.com>
2024-08-05 18:04:27 +0200

    display notes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
-function App() {
-  return <></>
+/* eslint-disable react/prop-types */
+
+function App({ store }) {
+  const notes = store.getState()
+
+  return (
+    <>
+      <ul>
+        {notes.map((note) => (
+          <li key={note.id}>
+            {note.content} {note.important ? <strong>important</strong> : null}
+          </li>
+        ))}
+      </ul>
+    </>
+  )
 }
 
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -17,6 +17,23 @@ const noteReducer = (state = [], action) => {
 
 const store = createStore(noteReducer)
 
+store.dispatch({
+  type: 'NEW_NOTE',
+  payload: {
+    content: 'the app state is in redux store',
+    important: true,
+    id: 1,
+  },
+})
+store.dispatch({
+  type: 'NEW_NOTE',
+  payload: {
+    content: 'state changes are made with actions',
+    important: false,
+    id: 2,
+  },
+})
+
 const root = ReactDOM.createRoot(document.getElementById('root'))
 const render = () =>
   root.render(
