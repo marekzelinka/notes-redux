f1e426d58b16cf26742d871186873df475d26332
Marek Zelinka <mzelinka17@gmail.com>
2024-08-06 08:22:58 +0200

    refactor to action creators

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,36 @@
 /* eslint-disable react/prop-types */
 
+import { newNote, toggleImportance } from './reducer'
+
 function App({ store }) {
   const notes = store.getState()
 
   return (
     <>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault()
+
+          const form = event.target
+          const formData = new FormData(form)
+
+          const content = formData.get('content')?.toString()
+          store.dispatch(newNote(content))
+
+          form.reset()
+          form.elements.content?.focus()
+        }}
+      >
+        <input type="text" name="content" id="content" aria-label="New note" />
+        <button type="submit">add</button>
+      </form>
       <ul>
         {notes.map((note) => (
           <li key={note.id}>
-            {note.content} {note.important ? <strong>important</strong> : null}
+            {note.content}{' '}
+            <button onClick={() => store.dispatch(toggleImportance(note.id))}>
+              {note.important ? 'make not important' : 'make important'}
+            </button>
           </li>
         ))}
       </ul>
--- src/reducer.js
+++ src/reducer.js
@@ -1,4 +1,4 @@
-export const noteReducer = (state = [], action) => {
+export function noteReducer(state = [], action) {
   switch (action.type) {
     case 'NEW_NOTE': {
       return state.concat(action.payload)
@@ -14,4 +14,28 @@ export const noteReducer = (state = [], action) => {
       return state
     }
   }
+}
+
+export function newNote(content) {
+  return {
+    type: 'NEW_NOTE',
+    payload: {
+      content,
+      important: false,
+      id: generateId(),
+    },
+  }
+}
+
+export function toggleImportance(id) {
+  return {
+    type: 'TOGGLE_IMPORTANCE',
+    payload: {
+      id,
+    },
+  }
+}
+
+function generateId() {
+  return Math.floor(Math.random() * 1_000_000)
 }
